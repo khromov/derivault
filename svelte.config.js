ca600d7790759bcc4130a08caa4ef3bc57561c29
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-01-03 03:18:32 +0100

    Revert "Use 404.html for fallback"
    This reverts commit 7a3122367093f25e5e4530815c99d34b7fe79e64.

--- svelte.config.js
+++ svelte.config.js
@@ -9,19 +9,19 @@ const config = {
 
 	kit: {
 		version: {
-			pollInterval: 0
+			pollInterval: 0,
 		},
 		output: {
-			bundleStrategy: 'inline'
+			bundleStrategy: 'inline',
 		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: '404.html'
+			fallback: 'index.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/derivault' : ''
+			base: process.env.NODE_ENV === 'production' ? '/derivault' : '',
 		}
 	}
 };
