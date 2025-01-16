3dd8323fc7291a353dddf12b5bd65685ad70e4c1
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-01-16 22:18:49 +0100

    generate password on-demand

--- src/routes/(app)/vault/+page.ts
+++ src/routes/(app)/vault/+page.ts
@@ -1,7 +1,6 @@
 import type { PageLoad } from './$types';
 import { get } from 'svelte/store';
 import { sites } from '$lib/stores';
-import { generatePassword } from '$lib/crypto';
 import { error } from '@sveltejs/kit';
 
 export const load: PageLoad = async ({ parent }) => {
@@ -12,14 +11,7 @@ export const load: PageLoad = async ({ parent }) => {
 		error(400, 'Encryption key not available');
 	}
 
-	const passwords = await Promise.all(
-		allSites.map(async (site) => {
-			const password = await generatePassword(derivedKey, site);
-			return { ...site, password };
-		})
-	);
-
 	return {
-		sites: passwords
+		sites: allSites
 	};
 };
