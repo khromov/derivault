dd5d01af3170471431510626cd70adbafde3be3f
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-01-17 00:32:22 +0100

    Update DeletionButton.svelte

--- src/lib/components/DeletionButton.svelte
+++ src/lib/components/DeletionButton.svelte
@@ -53,24 +53,33 @@
 	});
 </script>
 
-<button
-	class="relative overflow-hidden rounded bg-red-500 p-2 text-white transition-colors hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
-	on:mousedown={handleMouseDown}
-	on:mouseup={handleMouseUp}
-	on:mouseleave={handleMouseUp}
-	on:touchstart|preventDefault={handleMouseDown}
-	on:touchend|preventDefault={handleMouseUp}
->
-	<div class="relative z-10">
-		<Trash2 size={16} />
-	</div>
+<div class="relative">
 	{#if pressed}
 		<div
-			class="absolute bottom-0 left-0 right-0 bg-red-700 transition-none"
-			style="height: {progress * 100}%;"
-		></div>
+			class="absolute bottom-[calc(100%+4px)] left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-black/75 px-2 py-1 text-xs text-white"
+		>
+			Hold to delete ({Math.round(progress * 100)}%)
+		</div>
 	{/if}
-</button>
+	<button
+		class="relative overflow-hidden rounded bg-red-500 p-2 text-white transition-colors hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+		on:mousedown={handleMouseDown}
+		on:mouseup={handleMouseUp}
+		on:mouseleave={handleMouseUp}
+		on:touchstart|preventDefault={handleMouseDown}
+		on:touchend|preventDefault={handleMouseUp}
+	>
+		<div class="relative z-10">
+			<Trash2 size={16} />
+		</div>
+		{#if pressed}
+			<div
+				class="absolute bottom-0 left-0 right-0 bg-red-700 transition-none"
+				style="height: {progress * 100}%;"
+			/>
+		{/if}
+	</button>
+</div>
 
 <style>
 	button {
