9b3f4b5e35eb16c8cd0148603055ae1be21e0497
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-03-30 21:56:15 +0200

    apply formatting

--- svelte.config.js
+++ svelte.config.js
@@ -9,10 +9,10 @@ const config = {
 
 	kit: {
 		version: {
-			pollInterval: 0,
+			pollInterval: 0
 		},
 		output: {
-			bundleStrategy: 'inline',
+			bundleStrategy: 'inline'
 		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
@@ -21,7 +21,7 @@ const config = {
 			fallback: 'index.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/derivault' : '',
+			base: process.env.NODE_ENV === 'production' ? '/derivault' : ''
 		}
 	}
 };
